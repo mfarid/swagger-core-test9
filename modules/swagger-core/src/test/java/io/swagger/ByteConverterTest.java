ee223bbce03164d70d5c6ee8c10436e067b9ab28
Ubuntu <ubuntu@ip-10-206-0-132.ec2.internal>
2016-06-22 16:53:34 +0000

    first commit

---
+++ modules/swagger-core/src/test/java/io/swagger/ByteConverterTest.java
@@ -0,0 +1,92 @@
+package io.swagger;
+
+import io.swagger.converter.ModelConverters;
+import io.swagger.matchers.SerializationMatchers;
+import io.swagger.models.Model;
+
+import io.swagger.models.ModelImpl;
+import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.properties.BinaryProperty;
+import io.swagger.models.properties.ByteArrayProperty;
+import io.swagger.util.Json;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static org.testng.Assert.assertEquals;
+
+public class ByteConverterTest {
+    private static final String NEWLINE = System.getProperty("line.separator");
+    
+    @Test
+    public void testByte() {
+        final Map<String, Model> models = ModelConverters.getInstance().read(ByteConverterModel.class);
+        final String json = "{" +
+                "   \"ByteConverterModel\":{" +
+                "      \"type\":\"object\"," +
+                "      \"properties\":{" +
+                "         \"myBytes\":{" +
+                "            \"type\":\"array\"," +
+                "            \"items\":{" +
+                "               \"type\":\"string\"," +
+                "               \"format\":\"byte\"" +
+                "            }" +
+                "         }" +
+                "      }" +
+                "   }" +
+                "}";
+        SerializationMatchers.assertEqualsToJson(models, json);
+    }
+
+    @Test
+    public void testByteProperty() {
+        Model model = new ModelImpl()
+                .property("byteProperty", new ByteArrayProperty());
+
+        assertEquals(Json.pretty(model), "{" + NEWLINE +
+                "  \"properties\" : {" + NEWLINE +
+                "    \"byteProperty\" : {" + NEWLINE +
+                "      \"type\" : \"string\"," + NEWLINE +
+                "      \"format\" : \"byte\"" + NEWLINE +
+                "    }" + NEWLINE +
+                "  }" + NEWLINE +
+                "}");
+    }
+
+    @Test
+    public void testDeserializeByteProperty() throws Exception {
+        String json = "{\n" +
+                "  \"properties\" : {\n" +
+                "    \"byteProperty\" : {\n" +
+                "      \"type\" : \"string\",\n" +
+                "      \"format\" : \"byte\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+
+        Model model = Json.mapper().readValue(json, Model.class);
+        Json.prettyPrint(model);
+    }
+
+    @Test
+    public void testByteArray() {
+        Model model = new ModelImpl()
+                .property("byteArray", new ArrayProperty(new BinaryProperty()));
+
+        assertEquals(Json.pretty(model), "{" + NEWLINE +
+                "  \"properties\" : {" + NEWLINE +
+                "    \"byteArray\" : {" + NEWLINE +
+                "      \"type\" : \"array\"," + NEWLINE +
+                "      \"items\" : {" + NEWLINE +
+                "        \"type\" : \"string\"," + NEWLINE +
+                "        \"format\" : \"binary\"" + NEWLINE +
+                "      }" + NEWLINE +
+                "    }" + NEWLINE +
+                "  }" + NEWLINE +
+                     "}");
+    }
+
+    class ByteConverterModel {
+        public Byte[] myBytes = new Byte[0];
+    }
+}
